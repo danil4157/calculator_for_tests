f9bce32d9943777e1185899e3d1fdf912db2f291
danil4157 <iameagle@protonmail.com>
2016-10-28 13:32:41 +0400

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,7 @@
+double absolute (float a);
+double summary(float a, float b);
+double subtraction(float a, float b);
+double multiplication(float a, float b);
+double division(float a, float b);
+double involution(float a, int c);
+double square_root(float a); 
